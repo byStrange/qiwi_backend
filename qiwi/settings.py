e1acf718e17a66406896afab0365c668fee4583b
Rakhmatullo <qosimovrahmatullo006@outlook.com>
2023-07-13 10:56:43 +0500

    v.1.0.53

--- qiwi/settings.py
+++ qiwi/settings.py
@@ -152,8 +152,9 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "config": {
-            "hosts":  [os.environ.get("REDIS_TLS_URL", "redis://localhost:6379")]
-}
+            "hosts":  [os.environ.get("REDIS_URL", "redis://localhost:6379")]
+	},
+	"ROUTING":  "chat.routing.websocket_urlpatterns"
     },
 }
 
